94f07cf55d29c7adb29cecb2ec397ee8c6a5038f
Johnny Wu <KarasAya@gmail.com>
2015-06-19 14:54:47 +0800

    remove unused code

--- panel/scene.js
+++ panel/scene.js
@@ -4,21 +4,10 @@ var Util = require('util');
 Editor.registerPanel( 'scene.panel', {
     is: 'editor-scene',
 
-    listeners: {
-        'resize': '_onResize',
-    },
-
     properties: {
     },
 
     ready: function () {
-    },
-
-    _onResize: function ( event ) {
-        // this.$.view.send('resize')
-        // var bcr = this.$.webviewWrapper.getBoundingClientRect();
-        // this.$.view.setAttribute('minwidth', bcr.width);
-        // this.$.view.setAttribute('minheight', bcr.height);
     },
 });
 
