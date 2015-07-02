9960fad57fd805402dbfe212dbc2d3aa3179eedf
Jare Guo <jare85@163.com>
2015-07-02 18:18:17 +0800

    add pixi-grid

--- panel/webview/scene-view.js
+++ panel/webview/scene-view.js
@@ -12,14 +12,32 @@ Polymer( {
         requestAnimationFrame( function () {
             this._initEngine();
         }.bind(this));
+
+        // grid
+        this.$.grid.setScaleH( [5,2,3,2], 1, 1000, 'frame' );
+        // this.$.grid.setScaleH( [5,2], 0.01, 1000 );
+        this.$.grid.setMappingH( 0, 100, 100 );
+
+        this.$.grid.setScaleV( [5,2], 0.01, 1000 );
+        this.$.grid.setMappingV( 100, -100, 200 );
+
+        this.$.grid.setAnchor( 0.0, 0.5 );
+    },
+
+    _resize: function () {
+        this.$.grid.resize();
+        this.$.grid.repaint();
     },
 
     _initEngine: function () {
+        // init grid
+        this._resize();
+
         // init asset library
         Fire.AssetLibrary.init(Editor.importPath);
 
         // init engine
-        var canvasEL = this.$.canvas;
+        var canvasEL = this.$['runtime-canvas'];
         var bcr = this.getBoundingClientRect();
         canvasEL.width  = bcr.width;
         canvasEL.height = bcr.height;
@@ -60,6 +78,7 @@ Polymer( {
                 _resizeDebounceID = null;
                 var bcr = self.getBoundingClientRect();
                 Fire.engine.canvasSize = new Fire.v2( bcr.width, bcr.height );
+                self._resize();
             }, 10);
         });
     },
