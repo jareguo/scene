280704297d2e2bca929828f1522c38a570e860e7
2youyouo2 <501251991@qq.com>
2015-07-08 18:55:49 +0800

    fixed errors when drag items in hierarchy (fireball-x/runtime-cocos2d-js#44)

--- panel/webview/ipc.js
+++ panel/webview/ipc.js
@@ -145,8 +145,12 @@ Ipc.on('scene:node-mixin', function ( id, uuid ) {
 });
 
 Ipc.on('scene:move-nodes', function ( ids, parentID, nextSiblingId ) {
-    var parent = parentID && Fire.node(Fire.engine.getRuntimeInstanceById(parentID));
-    parent = parent || Fire.engine.getCurrentScene();
+    var parent;
+
+    if (parentID)
+        parent = Fire.node(Fire.engine.getRuntimeInstanceById(parentID));
+    else
+        parent = Fire.engine.getCurrentScene();
 
     var next = nextSiblingId ? Fire.node(Fire.engine.getRuntimeInstanceById(nextSiblingId)) : null;
     var nextIndex = next ? next.getSiblingIndex() : -1;
