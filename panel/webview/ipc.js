bc809394cdc1b683f772406a5ef3945a1728c7be
Johnny Wu <KarasAya@gmail.com>
2015-08-23 09:31:04 +0800

    update fireball-x/fireball#242 avoid setting design resolution in scene view

--- panel/webview/ipc.js
+++ panel/webview/ipc.js
@@ -422,7 +422,6 @@ Ipc.on('scene:init-scene-view', function ( settings ) {
     window.sceneView.$.gizmosView.coordinate = settings.coordinate;
     window.sceneView.$.gizmosView.pivot = settings.pivot;
     window.sceneView.$.gizmosView.designSize = [settings.designWidth, settings.designHeight];
-    Fire.engine.designResolution = Fire.v2(settings.designWidth, settings.designHeight);
 });
 
 Ipc.on('scene:transform-tool-changed', function ( value ) {
@@ -442,6 +441,5 @@ Ipc.on('scene:pivot-changed', function ( value ) {
 
 Ipc.on('scene:design-size-changed', function ( w, h ) {
     window.sceneView.$.gizmosView.designSize = [w, h];
-    Fire.engine.designResolution = Fire.v2(w,h);
     Fire.engine.repaintInEditMode();
 });
